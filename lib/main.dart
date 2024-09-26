10ef83f3e4f5ae0e8cfaa48dfba7fee2116e0ec8
Davipcrs <davi.coelho_comp@outlook.com>
2024-09-25 23:04:54 -0300

    new import

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -32,7 +31,7 @@ class MyApp extends StatelessWidget {
         brightness: Brightness.dark,
       ),
       home: Scaffold(
-        body: SafeArea(child: NoteMainWindow()),
+        body: const SafeArea(child: NoteMainWindow()),
         appBar: customAppBar(context),
       ),
     );
